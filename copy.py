70903406839463f66633878fb706590ef723de66
jtremblay <jtremblay@localhost>
2013-02-12 09:30:31 -0500

    remove flashing from copy simplify paste

--- copy.py
+++ copy.py
@@ -1,25 +1,11 @@
-# Get anchors and caret position from current selection
-caret_pos = editor.getCurrentPos()
-sel_start = editor.getSelectionStart()
-sel_end = editor.getSelectionEnd()
+# Get first and last line selected
+first_line = editor.lineFromPosition(editor.getSelectionStart())
+last_line = editor.lineFromPosition(editor.getSelectionEnd())
 
-# Retrieve position of start of line where left anchor is
-editor.setCurrentPos(sel_start)
-editor.home()
-start = editor.getCurrentPos()
-
-# Retrieve position of start of line under where right anchor is to copy complete line (characters + EOL)
-editor.setCurrentPos(sel_end)
-editor.home()
-editor.lineDown()
-end = editor.getCurrentPos()
-
-# Select all lines and copy it to clipboard
-editor.setSelectionStart(start)
-editor.setSelectionEnd(end)
-editor.copy()
-
-# Replace anchors and caret at original position
-editor.setSelectionStart(sel_start)
-editor.setSelectionEnd(sel_end)
-editor.setCurrentPos(caret_pos)
+# Copy all lines selected
+text = ""
+for i in range(first_line, last_line + 1):
+	text += editor.getLine(i)
+	
+# Write lines in clipboard
+editor.copyText(text)
--- paste.py
+++ paste.py
@@ -1,15 +1,11 @@
-# Get left anchor position from current selection
-sel_start = editor.getSelectionStart()
-
 # Retrieve position of start of line where left anchor is
-editor.setCurrentPos(sel_start)
+editor.setCurrentPos(editor.getSelectionStart())
 editor.home()
-start = editor.getCurrentPos()
 
 # Unselect all lines and set caret to start of line where left anchor was
-editor.setSelectionStart(start)
-editor.setSelectionEnd(start)
-editor.setCurrentPos(start)
+editor.setSelectionStart(editor.getCurrentPos())
+editor.setSelectionEnd(editor.getCurrentPos())
+editor.setCurrentPos(editor.getCurrentPos())
 
 # Paste text coming from clipboard
 editor.paste()
